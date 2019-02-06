280ea0043ba249f22df5eed9560e815d971010c6
jamesliudotcc <james@jamesliu.cc>
2019-02-06 15:44:18 -0800

    changed env variables to no slashes

--- index.js
+++ index.js
@@ -1,168 +1,70 @@
-'use strict';
-var __awaiter =
-  (this && this.__awaiter) ||
-  function(thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function(resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator['throw'](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : new P(function(resolve) {
-              resolve(result.value);
-            }).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __generator =
-  (this && this.__generator) ||
-  function(thisArg, body) {
-    var _ = {
-        label: 0,
-        sent: function() {
-          if (t[0] & 1) throw t[1];
-          return t[1];
-        },
-        trys: [],
-        ops: [],
-      },
-      f,
-      y,
-      t,
-      g;
-    return (
-      (g = { next: verb(0), throw: verb(1), return: verb(2) }),
-      typeof Symbol === 'function' &&
-        (g[Symbol.iterator] = function() {
-          return this;
-        }),
-      g
-    );
-    function verb(n) {
-      return function(v) {
-        return step([n, v]);
-      };
-    }
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
-      if (f) throw new TypeError('Generator is already executing.');
-      while (_)
-        try {
-          if (
-            ((f = 1),
-            y &&
-              (t =
-                op[0] & 2
-                  ? y['return']
-                  : op[0]
-                  ? y['throw'] || ((t = y['return']) && t.call(y), 0)
-                  : y.next) &&
-              !(t = t.call(y, op[1])).done)
-          )
-            return t;
-          if (((y = 0), t)) op = [op[0] & 2, t.value];
-          switch (op[0]) {
-            case 0:
-            case 1:
-              t = op;
-              break;
-            case 4:
-              _.label++;
-              return { value: op[1], done: false };
-            case 5:
-              _.label++;
-              y = op[1];
-              op = [0];
-              continue;
-            case 7:
-              op = _.ops.pop();
-              _.trys.pop();
-              continue;
-            default:
-              if (
-                !((t = _.trys), (t = t.length > 0 && t[t.length - 1])) &&
-                (op[0] === 6 || op[0] === 2)
-              ) {
-                _ = 0;
-                continue;
-              }
-              if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
-                _.label = op[1];
-                break;
-              }
-              if (op[0] === 6 && _.label < t[1]) {
-                _.label = t[1];
-                t = op;
-                break;
-              }
-              if (t && _.label < t[2]) {
-                _.label = t[2];
-                _.ops.push(op);
-                break;
-              }
-              if (t[2]) _.ops.pop();
-              _.trys.pop();
-              continue;
-          }
-          op = body.call(thisArg, _);
-        } catch (e) {
-          op = [6, e];
-          y = 0;
-        } finally {
-          f = t = 0;
-        }
-      if (op[0] & 5) throw op[1];
-      return { value: op[0] ? op[1] : void 0, done: true };
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
-  };
+};
 var _this = this;
-Object.defineProperty(exports, '__esModule', { value: true });
+Object.defineProperty(exports, "__esModule", { value: true });
 // Express required Imports
-var cors = require('cors');
-var dotenv_1 = require('dotenv');
-var express = require('express');
-var logger = require('morgan');
+var cors = require("cors");
+var dotenv_1 = require("dotenv");
+var express = require("express");
+var logger = require("morgan");
 dotenv_1.config();
 // DB required imports
-require('reflect-metadata');
-var typeorm_1 = require('typeorm');
-var Assignment_1 = require('./entity/Assignment');
-var Cohort_1 = require('./entity/Cohort');
-var Deliverable_1 = require('./entity/Deliverable');
-var Topic_1 = require('./entity/Topic');
-var User_1 = require('./entity/User');
-// End of upload required packages
-typeorm_1
-  .createConnection({
+require("reflect-metadata");
+var typeorm_1 = require("typeorm");
+var Assignment_1 = require("./entity/Assignment");
+var Cohort_1 = require("./entity/Cohort");
+var Deliverable_1 = require("./entity/Deliverable");
+var Topic_1 = require("./entity/Topic");
+var User_1 = require("./entity/User");
+typeorm_1.createConnection({
     type: 'mongodb',
     host: process.env.MONGODB_URI,
-    entities: [
-      Assignment_1.Assignment,
-      Cohort_1.Cohort,
-      Deliverable_1.Deliverable,
-      Topic_1.Topic,
-      User_1.User,
-    ],
+    port: Number(process.env.MONGODB_PORT),
+    database: process.env.MONGODB_DB_NAME,
+    entities: [Assignment_1.Assignment, Cohort_1.Cohort, Deliverable_1.Deliverable, Topic_1.Topic, User_1.User],
+    username: process.env.MONGODB_USERNAME,
+    password: process.env.MONGODB_PASSWORD,
     useNewUrlParser: true,
     synchronize: true,
     logging: false,
-  })
-  .then(function(connection) {
-    return __awaiter(_this, void 0, void 0, function() {
-      var userRepository, app, listenPort;
-      return __generator(this, function(_a) {
+})
+    .then(function (connection) { return __awaiter(_this, void 0, void 0, function () {
+    var userRepository, app, listenPort;
+    return __generator(this, function (_a) {
         userRepository = connection.getRepository(User_1.User);
         app = express();
         /* ****************************************
@@ -177,18 +79,17 @@ typeorm_1
         ******************************************/
         app.use('/auth', require('./controllers/auth'));
         app.use('/cohort', require('./controllers/cohort'));
-        app.get('*', function(req, res, next) {
-          res.status(404).send({ message: 'Not Found' });
+        app.get('*', function (req, res, next) {
+            res.status(404).send({ message: 'Not Found' });
         });
         listenPort = process.env.PORT || 3000;
-        app.listen(listenPort, function() {
-          console.log('Listening at port ' + listenPort);
+        app.listen(listenPort, function () {
+            console.log("Listening at port " + listenPort);
         });
         return [2 /*return*/];
-      });
     });
-  })
-  .catch(function(error) {
+}); })
+    .catch(function (error) {
     console.log(error);
-  });
+});
 //# sourceMappingURL=index.js.map
